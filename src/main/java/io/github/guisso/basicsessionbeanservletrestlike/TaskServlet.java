52a218b5e5c9c11893a2d6bb16cb108217f3c146
Guisso <luis.guisso@ifnmg.edu.br>
2025-09-19 09:06:03 -0300

    Basic response tests for each HTTP verb

--- src/main/java/io/github/guisso/basicsessionbeanservletrestlike/TaskServlet.java
+++ src/main/java/io/github/guisso/basicsessionbeanservletrestlike/TaskServlet.java
@@ -24,7 +24,6 @@
 package io.github.guisso.basicsessionbeanservletrestlike;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -42,32 +41,21 @@ import jakarta.servlet.http.HttpServletResponse;
 public class TaskServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TaskServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TaskServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        // Simple test
+        response.setContentType("text/plain");
+        response.getWriter().write("POST OK");
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -79,11 +67,13 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        // Simple test
+        response.setContentType("text/plain");
+        response.getWriter().write("GET OK");
     }
 
     /**
-     * Handles the HTTP <code>POST</code> method.
+     * Handles the HTTP <code>PUT</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -91,19 +81,27 @@ public class TaskServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        // Simple test
+        response.setContentType("text/plain");
+        response.getWriter().write("PUT OK");
     }
 
     /**
-     * Returns a short description of the servlet.
+     * Handles the HTTP <code>DELETE</code> method.
      *
-     * @return a String containing servlet description
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Simple test
+        response.setContentType("text/plain");
+        response.getWriter().write("DELETE OK");
+    }
 
 }
